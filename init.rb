64369cacbfe6e9ee871b9888ab4d5e63e9cf223e
Alexander Sapozhnikov <shoorick@mail.ru>
2010-10-20 11:57:44 +0600

    Added empty plugin

---
+++ init.rb
@@ -0,0 +1,10 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_goods_request do
+  name 'Redmine Goods Request plugin'
+  author 'Alexander Sapozhnikov'
+  description 'Create requests for goods as usual tasks'
+  version '0.0.1'
+  url 'http://github.com/shoorick/redmine_goods_request'
+  author_url 'http://shoorick.ru/'
+end
